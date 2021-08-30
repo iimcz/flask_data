6511618eb951dbef009b1ba0e89f47e93df9af2e
Ondřej Slabý <kron258@gmail.com>
2021-08-30 15:58:45 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setuptools.setup(
     description='A set of helper classes to act as a data middleman between a server/data file and Neos VR (or any other application)',
     author='Ondřej Slabý',
     author_email='slaby@iim.cz',
-    packages=setuptools.find_packages(['flask_data']),
+    packages=setuptools.find_packages(exclude='examples'),
     install_requirements=['flask', 'flask_sock', 'matplotlib', 'numpy']
 )
